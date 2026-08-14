116f358ab458b8a3e925b84f9ea0b1b3b97b058f
Radek90211 <radek90211@gmail.com>
2026-08-14 17:34:00 +0200

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -828,7 +828,7 @@ async function renderDashboard(user) {
       <iframe
         id="tms-frame"
         class="tms-frame is-loading"
-        src="/tms.html?embedded=1&build=fleet-supabase-v4"
+        src="/tms.html?embedded=1&build=fleet-supabase-v5-mapfix"
         title="Top Dragon TMS"
       ></iframe>
     </main>
