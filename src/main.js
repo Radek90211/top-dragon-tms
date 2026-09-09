ab29d1b13a5e429ae57669d33b2cd1fa6b3efc1f
Radek90211 <radek90211@gmail.com>
2026-09-09 22:31:18 +0200

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -4209,7 +4209,7 @@ async function renderDashboard(user) {
       <iframe
         id="tms-frame"
         class="tms-frame is-loading"
-          src="/tms.html?embedded=1&build=request-workflow-v131-queue-admin-tabs"
+          src="/tms.html?embedded=1&build=request-workflow-v133-lower-tabs-full-panels"
         title="Top Dragon TMS"
       ></iframe>
     </main>
